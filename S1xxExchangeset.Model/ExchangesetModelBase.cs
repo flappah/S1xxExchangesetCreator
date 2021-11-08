6650b89cd71bda0e4f0659c04074594f7fdf1a24
Richard Flapper <richard_flapper@hotmail.com>
2021-11-08 16:41:41 +0100

    Added models that parse the individual S1xx files to generate the exchangeset on

---
+++ S1xxExchangeset.Model/ExchangesetModelBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S1xxExchangeset.Model
+{
+    public abstract class ExchangesetModelBase
+    {
+    }
+}
---
+++ S1xxExchangeset.Model/S122ExchangesetModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S1xxExchangeset.Model
+{
+    public class S122ExchangesetModel : ExchangesetModelBase
+    {
+    }
+}
